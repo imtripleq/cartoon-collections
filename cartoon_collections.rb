ebc14f9929c32fb53a89f03524ed502cf33300d4
imtripleq <flamework89@hotmail.com>
2022-01-22 11:42:18 +1100

    Done

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)
+  array.each_with_index { |word, index| puts "#{index + 1}. #{word}" }
+
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
+  array.map { |word| word.capitalize + '!' }
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  array.any? { |word| word.size > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types = %w[cheddar gouda camembert]
+
+  array.find { |cheese| cheese_types.include?(cheese) }
 end
